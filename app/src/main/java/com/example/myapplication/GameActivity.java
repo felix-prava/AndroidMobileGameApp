458d3facf06594434e7881f5af8c46e0acfb0a75
Felix <felix.prava@takeofflabs.com>
2021-12-02 23:19:11 +0200

    Add game design

--- app/src/main/java/com/example/myapplication/GameActivity.java
+++ app/src/main/java/com/example/myapplication/GameActivity.java
@@ -1,14 +1,34 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+
+    private ImageView bird, enemy1, enemy2, enemy3, coin1, coin2, heart1, heart2, heart3;
+    private TextView textViewScore, textViewStartInfo;
+    private ConstraintLayout constraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        bird = findViewById(R.id.imageViewBird);
+        enemy1 = findViewById(R.id.imageViewEnemy1);
+        enemy2 = findViewById(R.id.imageViewEnemy2);
+        enemy3 = findViewById(R.id.imageViewEnemy3);
+        coin1 = findViewById(R.id.imageViewCoin);
+        coin2 = findViewById(R.id.imageViewCoin2);
+        heart1 = findViewById(R.id.imageViewHeart1);
+        heart2 = findViewById(R.id.imageViewHeart2);
+        heart3 = findViewById(R.id.imageViewHeart3);
+        textViewScore = findViewById(R.id.textViewScore);
+        textViewStartInfo = findViewById(R.id.textViewStartInfo);
+        constraintLayout = findViewById(R.id.constraintLayout);
     }
 }
